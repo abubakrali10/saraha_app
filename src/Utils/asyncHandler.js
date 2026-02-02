f052029c1bc9ce56800590bda50da253da9342f9
abubakrali10 <bakrali175@yahoo.com>
2026-02-02 22:59:41 +0200

    handle async

---
+++ src/Utils/asyncHandler.js
@@ -0,0 +1,15 @@
+
+
+export const asyncHandler = (fn => {
+	return (req, res, next) => {
+		fn(req, res, next).catch((err) => {
+			return res
+			.status(500)
+			.json({
+				message: "Something went wrong!",
+				error: err.message,
+				stack: err.stack,
+			});
+		})
+	}
+})
