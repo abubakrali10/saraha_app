61fd2f239a6f8278015c2677c303094c7de19059
abubakrali10 <bakrali175@yahoo.com>
2026-02-02 23:01:00 +0200

    update function to add asyncHandler

--- src/Modules/Auth/auth.service.js
+++ src/Modules/Auth/auth.service.js
@@ -1,33 +1,25 @@
 import { UserModel } from "../../DB/Models/user.model.js";
+import { asyncHandler } from "../../Utils/asyncHandler.js";
 
-export const signup = async (req, res, next) => {
-	try {
-		const { firstName, lastName, email, password, gender, phoneNumber } = req.body;
+export const signup = asyncHandler(async (req, res, next) => {
+	const { firstName, lastName, email, password, gender, phoneNumber } = req.body;
 
-		const user = await UserModel.findOne({ email });
+	const user = await UserModel.findOne({ email });
 
-		if (user) return res.status(409).json({ message: "User already exists!" });
+	if (user) return res.status(409).json({ message: "User already exists!" });
 
-		const newUser = await UserModel.create({ firstName, lastName, email, password, gender, phoneNumber });
+	const newUser = await UserModel.create({ firstName, lastName, email, password, gender, phoneNumber });
 
-		return res.status(201).json({ message: "User registered successfuly!", user: newUser });
+	return res.status(201).json({ message: "User registered successfuly!", user: newUser });
+});
 
-	} catch (error) {
-		return res.status(500).json({ message: "Something went wrong!", error: error.message });
-	}
-}
+export const login = asyncHandler(async (req, res, next) => {
+	const { email, password } = req.body;
 
-export const login = async (req, res, next) => {
-	try {
-		const { email, password } = req.body;
+	const user = await UserModel.findOne({ email, password });
 
-		const user = await UserModel.findOne({ email, password });
+	if (!user) return res.status(404).json({ message: "User not found!" });
 
-		if (!user) return res.status(404).json({ message: "User not found!" });
+	return res.status(200).json({ message: "User logged in successfuly!", user });
 
-		return res.status(200).json({ message: "User logged in successfuly!", user });
-
-	} catch (error) {
-		return res.status(500).json({ message: "Something went wrong!", error: error.message });
-	}
-}
+});
